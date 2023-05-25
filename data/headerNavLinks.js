dda46dd1173caeb1840416fa68f246c28b246bdc
tyler <tylerscott.home@gmail.com>
2023-05-25 09:28:33 -0400

    Removed 'blog' section

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   // { href: '/projects', title: 'Projects' },
-  { href: '/blog', title: 'Blog' },
+  // { href: '/blog', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' },
 ]
